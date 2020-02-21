d281c1a1cc622c2745ad77ba08997e1e2026ec56
MacBook Pro <macbookpro@autoreg-218646.dyn.wpi.edu>
2020-02-21 00:09:48 -0500

    implemented first version of recycler view

--- app/src/main/java/com/mealsharing/myapplication/FindMealActivity.java
+++ app/src/main/java/com/mealsharing/myapplication/FindMealActivity.java
@@ -1,7 +1,10 @@
 package com.mealsharing.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,6 +15,12 @@ public class FindMealActivity extends AppCompatActivity {
 
     private static final String TAG = "FINDMEALQUERY";
     private DatabaseReference mDatabaseReference;
+    RecyclerView recyclerView;
+
+    String test [] = {"im shine", "im chloe", "im alex", "im joe"};
+    int images [] = {R.drawable.com_facebook_profile_picture_blank_portrait,R.drawable.com_facebook_favicon_blue, R.drawable.common_google_signin_btn_icon_dark, R.drawable.com_facebook_profile_picture_blank_portrait};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,12 @@ public class FindMealActivity extends AppCompatActivity {
         mDatabaseReference.child("Meals");
         Log.d(TAG, "trying to debug");
 
+        recyclerView = findViewById(R.id.recycler_view);
 
+
+        MyAdapter myAdapter = new MyAdapter(this, test, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
