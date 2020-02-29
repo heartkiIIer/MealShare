81c1dccbeb55401cc13ff344e113d0ebe1094ec9
Chau <jachane99@gmail.com>
2020-02-28 20:27:44 -0500

    Get locations up on the db

--- app/src/main/java/com/mealsharing/myapplication/FindMealOptionsActivity.java
+++ app/src/main/java/com/mealsharing/myapplication/FindMealOptionsActivity.java
@@ -1,12 +1,30 @@
 package com.mealsharing.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FindMealOptionsActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +41,61 @@ public class FindMealOptionsActivity extends AppCompatActivity {
 
 
     }
+
+    public void onMapButton(View view){
+        // Check GPS is enabled
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Check location permission is granted - if it is, start
+        // the service, otherwise request the permission
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("PERMISSION GRANTED");
+            Toast.makeText(this, "PERMISSION SERVICE ACCEPTED", Toast.LENGTH_SHORT);
+//            startTrackerService();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+        startTrackerService();
+
+
+    }
+    private void startTrackerService() {
+        startService(new Intent(this, TrackerService.class));
+        finish();
+    }
+//
+//    private void requestLocationUpdates() {
+//        LocationRequest request = new LocationRequest();
+//        request.setInterval(10000);
+//        request.setFastestInterval(5000);
+//        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+//        final String path = getString(R.string.firebase_path) + "/" + getString(R.string.transport_id);
+//        int permission = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION);
+//        if (permission == PackageManager.PERMISSION_GRANTED) {
+//            // Request location updates and when an update is
+//            // received, store the location in Firebase
+//            client.requestLocationUpdates(request, new LocationCallback() {
+//                @Override
+//                public void onLocationResult(LocationResult locationResult) {
+//                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+//                    Location location = locationResult.getLastLocation();
+//                    if (location != null) {
+//                        Log.d(TAG, "location update " + location);
+//                        ref.setValue(location);
+//                    }
+//                }
+//            }, null);
+//        }
+//    }
 
 }
--- app/src/main/java/com/mealsharing/myapplication/MainActivity.java
+++ app/src/main/java/com/mealsharing/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.mealsharing.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity
     private String mUsername;
     public static final String ANONYMOUS = "anonymous";
 
+    private static final int PERMISSIONS_REQUEST = 1;
 
 
     @Override
@@ -61,7 +68,28 @@ public class MainActivity extends AppCompatActivity
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
+
+        // Check GPS is enabled
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+            finish();
         }
+
+        // Check location permission is granted - if it is, start
+        // the service, otherwise request the permission
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("PERMISSION GRANTED");
+            Toast.makeText(this, "PERMISSION SERVICE ACCEPTED", Toast.LENGTH_SHORT);
+//            startTrackerService();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+    }
 
 //    Button ShareMeal = (Button) findViewById(R.id.SharingMeal);
 
